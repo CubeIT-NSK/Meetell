bcc06ced56ea6d2eb8015b86e42c4fcb1f1c90bb
melodeep <melodeep@student.21-school.ru>
2024-06-08 15:39:55 +0700

    fixed heigth

--- frontend/meetell_app/src/components/appFooter/Footer.js
+++ frontend/meetell_app/src/components/appFooter/Footer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import invite from '../../img/invite_footer.svg'
 import './Footer.css';
