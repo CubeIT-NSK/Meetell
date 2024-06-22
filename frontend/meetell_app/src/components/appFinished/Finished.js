b836955421e918b2173047ea0255914b1f7be240
Albert <Albert@mail.com>
2024-06-23 04:59:07 +0700

    pre full finished module

--- frontend/meetell_app/src/components/appFinished/Finished.js
+++ frontend/meetell_app/src/components/appFinished/Finished.js
@@ -21,7 +21,7 @@ export default function Finished() {
 
     return (
         <div className="Background" ref={parrentRef}>
-            <div className="Feedback-content">
+            {/* <div className="Feedback-content">
                 <div className="Title-text">
                     <h3>Удалось ли посетить данный маршрут?</h3>
                     <p>Поделиться маршрутом</p>
@@ -31,15 +31,56 @@ export default function Finished() {
                     <button className="No">Нет</button>
                 </div>
                 <img className="map" src={phone} />
-            </div>
+            </div> */}
+
+            {/* <div className="no-walk">
+                <p>Этот маршрут не будет учитываться в вашем профиле</p>
+                <img className="map" src={phone} />
+            </div> */}
+
             <div className="walk-assessment">
                 <div className="title-text-assessment">
                     <p>Рады узнать!</p>
                     <p>Отметили этот маршрут в вашем профиле.</p>
                 </div>
-                <div className="walk-grade">
+                <div className="thanks-for-gread">
+                    <p>Спасибо за обратную связь!</p>
+                    <p>Мы учтём вашу оченку при построении следующих маршрутов.</p>
+                </div>
+                <div className="grade-companions">
+                    <p className="header-grade-companions">Оцените своих спутников :)</p>
+                    <div className="full-data-companion">
+                        <div className="grade-companions-faces">
+                            <div className="grade-companions-logo" id="#">
+                                <img className="companions-avatar" src={account} alt="avatar"></img>
+                                <div className="companions-level">123</div>
+                            </div>
+                        </div>
+                        <div className="user-data-position">
+                            <div className="user-data-companion">
+                                <p>Красный рейнджер</p>
+                                <div className="user-age-sex">
+                                    <p>15</p>
+                                    <img src={sex} />
+                                </div>
+                            </div>
+                            <div className="grade-faces-thanks">
+                                <p>Спасибо за обратную связь!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+            {/* <div className="walk-assessment">
+                <div className="title-text-assessment">
+                    <p>Рады узнать!</p>
+                    <p>Отметили этот маршрут в вашем профиле.</p>
+                </div> */}
+               {/* <div className="walk-grade">
                     <h3>Оцените как всё прошло:</h3>
-                    <div className="grade-faces">
+                    <div className="grade-faces-big">
                         <img src={bad} />
                         <img src={sad} />
                         <img src={medium} />
@@ -49,30 +90,34 @@ export default function Finished() {
                     <button className="grade-submit">Оценить</button>
                 </div>
                 <div className="grade-companions">
-                    <p>Оцените своих спутников :)</p>
-                    <div className="grade-companions-faces">
-                        <div className="grade-companions-logo" id="#">
-                            <img className="companions-avatar" src={account} alt="avatar"></img>
-                            <div className="companions-level">123</div>
+                    <p className="header-grade-companions">Оцените своих спутников :)</p>
+                    <div className="full-data-companion">
+                        <div className="grade-companions-faces">
+                            <div className="grade-companions-logo" id="#">
+                                <img className="companions-avatar" src={account} alt="avatar"></img>
+                                <div className="companions-level">123</div>
+                            </div>
                         </div>
-                        <p>Красный рейнджер</p>
-                        <div className="user-age-sex">
-                            <p>15</p>
-                            <img src={sex} />
-                        </div>
-                        <div className="grade-faces">
-                            <img src={bad} />
-                            <img src={sad} />
-                            <img src={medium} />
-                            <img src={good} />
-                            <img src={funny} />
+                        <div className="user-data-position">
+                            <div className="user-data-companion">
+                                <p>Красный рейнджер</p>
+                                <div className="user-age-sex">
+                                    <p>15</p>
+                                    <img src={sex} />
+                                </div>
+                            </div>
+                            <div className="grade-faces">
+                                <img src={bad} />
+                                <img className="face-states" src={sad} />
+                                <img src={medium} />
+                                <img src={good} />
+                                <img src={funny} />
+                            </div>
                         </div>
                     </div>
-                    
                 </div>
-            </div>            
+            </div> */}           
         </div>
-        
     );
 
 }
