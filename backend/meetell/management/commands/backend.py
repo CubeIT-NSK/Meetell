3d8b3172db6c0a737fa1de80526a381cd5a7e5c8
joselynr <joselynr@student.21-school.ru>
2024-07-01 16:34:23 +0700

    backend 99% ready

--- backend/meetell/management/commands/backend.py
+++ backend/meetell/management/commands/backend.py
@@ -1,8 +1,22 @@
 from django.core.management.base import BaseCommand
 
+from datetime import datetime
+from ...models import TripUser, StateSelection
+
 class Command(BaseCommand):
   	# Используется как описание команды обычно
     help = 'Start checker base'
 
     def handle(self, *args, **kwargs):
-        pass
+        while True:
+          now = datetime.now()
+
+          # Выборка всех записей TripUser
+          trip_users = TripUser.objects.select_related('trip').filter(state = "W")
+
+          # Проверка и обновление состояния
+          for trip_user in trip_users:
+              if trip_user.trip.date < now:
+                  trip_user.state = StateSelection.QUEST
+                  trip_user.save()
+                  print(f"Updated TripUser {trip_user.id} to state 'Q'.")
--- backend/meetell/management/commands/bot.py
+++ backend/meetell/management/commands/bot.py
@@ -1,12 +1,16 @@
 import asyncio
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from ...models import User, Friend, SexSelection
+from ...models import User, Friend, SexSelection, Trip, City
 
+from aiogram import F
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, CommandObject
-from aiogram.types import Message, WebAppInfo
+from aiogram.types import Message, WebAppInfo, ContentType
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+import pandas as pd
+import io
 
 dp = Dispatcher()
 bot = Bot(settings.TOKEN)
@@ -49,3 +53,34 @@ async def command_start_handler(message: Message, command: CommandObject) -> Non
     builder = InlineKeyboardBuilder()
     builder.button(text="MeeTell App", web_app=WebAppInfo(url="https://meetell.ru"))
     await message.answer(text="MeeTell", reply_markup=builder.as_markup())
+
+@dp.message(F.content_type == ContentType.DOCUMENT)
+async def handle_document(message: Message):
+   if message.document.mime_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+      file_info = await bot.get_file(message.document.file_id)
+      file = await bot.download_file(file_info.file_path)
+
+      # Чтение файла в DataFrame
+      df = pd.read_excel(io.BytesIO(file.read()))
+
+      # Сохранение данных в базу
+      for index, row in df.iterrows():
+         trip = await Trip.objects.acreate(
+               date=row['Дата'],
+               name=row['Название'],
+               sex=row['Пол'],
+               year_st=row['Возраст от'],
+               year_en=row['Возраст до'],
+               time_sp=row['Время в пути'],
+               distance=row['Дистанция'],
+               ratting=0.0,  # можно изменить в зависимости от нужд
+               chat_link=row['Чат'],
+               map_block=row['Карта'],
+               city=await City.objects.aget(id=row['Город'])  # Предполагается, что город идентифицируется по ID
+         )
+         await trip.asave()
+         
+
+      await message.reply("Данные успешно сохранены в базу данных.")
+   else:
+      await message.reply("Пожалуйста, отправьте файл в формате XLSX.")
